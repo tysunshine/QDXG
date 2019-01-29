117f65a62dc46ded9b58a01563fe41d7e76b0e81
sunshineTY <877430318@qq.com>
2019-01-29 18:51:49 +0800

    自定义事件。。有点鸡肋

--- jsFunction/006_事件/002_自定义事件/customEvent.js
+++ jsFunction/006_事件/002_自定义事件/customEvent.js
@@ -3,6 +3,8 @@
 	/*
 	要完成的事：
 	一个节点监听一中自定义事件，并且能够自己调用
+	结语：唔，搞完了一看咋好像直接target.fn = ... 也可以呀，好像还完爆我的这个
+	不过，还是留着，毕竟也是我敲了一下午的哈
 	*/
 
 	var customEvent = {
@@ -33,7 +35,7 @@
 					break;
 				}
 			}
-			if ( !hasEvent ) {
+			if ( !hasEvent && typeof fn === 'function' ) {
 				target.customevent[type].push(fn);
 			}
 		},
@@ -51,17 +53,45 @@
 
 			// 只有一个处理函数可以接收参数
 			if ( target.customevent[type].length == 1 ) {
-				console.log(1);
-
+				var params = [];
+				for ( var i = 2; i < arguments.length; i++ ) {
+					params.push(arguments[i]);
+				}
+				target.customevent[type][0].apply(target, params);
 
 			} else {
+				for ( var i = 0; i < target.customevent[type].length; i++ ) {
+					target.customevent[type][i].apply(target);
+				}
+			}
+		},
 
+		/**
+		 * 移除自定义事件
+		 * target 元素节点
+		 * type 自定义的事件名
+		 */
+		removeEvent: function (target, type, fn) {
+			if ( !target.customevent || !target.customevent[type] || target.customevent[type].length <= 0 ) {
+				return false;
+			}
+			
+			var handlers = target.customevent[type];
+			for ( var i = 0; i < handlers.length; i++ ) {
+				if ( handlers[i] === fn ) {
+					handlers.splice(i, 1);
+					break;
+				}
 			}
 
+			if ( handlers.length <= 0 ) {
+				delete target.customevent[type];
+			}
+
+			if ( JSON.stringify(target.customevent) == '{}' ) {
+				delete target.customevent;
+			}
 		}
-
-
-
 	}
 
 	window.customEvent = customEvent;
