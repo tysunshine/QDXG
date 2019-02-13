20c23e639efe95fc5681ce5a84fbcb86382374a0
sunshineTY <877430318@qq.com>
2019-02-13 17:17:26 +0800

    正则、动画

---
+++ cssEffect/004_动画/003_多种动画/class.js
@@ -0,0 +1,80 @@
+
+// 获取class节点
+function getByClass (className) {
+	var arr = [];
+	if ( document.querySelectorAll ) {
+		var aElm = document.querySelectorAll('.' + className);
+		for(var i=0; i<arr.length; i++) {
+			arr.push(aElm[i]);
+		}
+	} else {
+		var aElm = doc.getElementsByTagName("*");
+		
+		for(var i=0; i<aElm.length; i++) {
+			if(hasClass(aElm[i], className)) {
+				arr.push(aElm[i]);
+			}
+		}
+	}
+	return arr;
+}
+
+// 获取节点的class列表
+function getClassList (obj) {
+	var sClass = obj.className;
+	var aClass = sClass.split(' ');
+	var list = [];
+	aClass.forEach(function (item, index) {
+		if ( item ) {
+			list.push(item);
+		}
+	})
+	return list;
+}
+
+// 判断是否存在class
+function hasClass (obj, name) {
+	var sClass = obj.className;
+	var reg = new RegExp('\\b' + name + '\\b');
+	if ( reg.test(sClass) ) {
+		return true;
+	}
+	return false;
+}
+
+// 添加样式名
+function addClass (obj, name) {
+	var hasName = hasClass(obj, name);
+
+	if ( !hasName ) {
+		var aClass = getClassList(obj);
+		var sClass = '';
+		aClass.push(name);
+
+		aClass.forEach(function (item, index, arr) {
+			sClass += item;
+			if ( index < arr.length - 1) {
+				sClass += ' ';
+			}
+		})
+
+		obj.className = sClass;
+	}
+}
+
+// 删除样式名
+function delClass (obj, name) {
+	var hasName = hasClass(obj, name);
+
+	if ( hasName ) {
+		var sClass = obj.className;
+		var arr = sClass.split(name);
+		sClass = '';
+		arr.forEach(function (item) {
+			sClass += item;
+		})
+		sClass = sClass.replace(/\s+/g, ' ');
+		sClass = sClass.replace(/^\s|\s$/g, '');
+		obj.className = sClass;
+	}
+}
