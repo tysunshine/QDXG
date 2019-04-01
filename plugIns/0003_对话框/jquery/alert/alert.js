dc18375ec04adc616770b29986777b002564bc24
sunshineTY <877430318@qq.com>
2019-04-01 15:49:11 +0800

    弹框

---
+++ plugIns/0003_对话框/jquery/alert/alert.js
@@ -0,0 +1,112 @@
+(function (window, document) {
+
+	function Alert() {
+		var arg1 = arguments[0];
+		var arg2 = arguments[1];
+		var arg3 = arguments[2];
+
+		var ty1 = typeof arg1;
+		var ty2 = typeof arg2;
+		var ty3 = typeof arg3;
+
+		if (!arg1) {
+			return;
+		}
+
+		if (ty2 == 'object') {
+			arg3 = arg2;
+		}
+
+		if (ty2 != 'object' && ty3 != 'object') {
+			arg3 = {}
+		}
+
+		if (ty2 != 'string') {
+			arg2 = '提示';
+		}
+
+		this._init(arg1, arg2, arg3);
+	}
+
+	Alert.prototype = {
+		_init: function (msg, title, config) {
+			this.msg = msg;
+			this.title = title;
+			this.config = config;
+
+			this.oRoot = null;		// 根节点
+			this.oContent = null;	// 显示内容
+			this.oConfrim = null;	// 确定按钮
+
+			this._initParam();
+			this._initStructure();
+			this._initEvent();
+			this.show();
+		},
+
+		_initEvent: function () {
+			var _this = this;
+
+			// 背景点击
+			this.oRoot.on('click', function () {
+				_this.close();
+			})
+
+			// 内容点击
+			this.oContent.on('click', function (evt) {
+				evt.stopPropagation();
+			})
+
+			// 确定按钮点击
+			this.oConfirm.on('click', function () {
+				if (_this.config.confirmFn) {
+					_this.config.confirmFn.apply(_this);
+				} else {
+					_this.close();
+				}
+			})
+		},
+
+		_initParam: function () {
+			this.confirmText = this.config.confirmText || '确定';
+		},
+
+		_initStructure: function () {
+			this.oRoot = $(`<div class="alert-box _wrapper">
+						<div class="_content">
+						<div class="_header">${this.title}</div>
+						<div class="_message"><p>${this.msg}</p></div>
+						<div class="_footer">
+							<button class="_confirm-btn">${this.confirmText}</button>
+						</div>
+					</div>
+				</div>`);
+			this.oContent = this.oRoot.find('._content');
+			this.oConfirm = this.oRoot.find('._confirm-btn');
+			$('body').append(this.oRoot);
+		},
+
+		show: function () {
+			var _this = this;
+			new Delay(function () {
+				_this.oRoot.css({
+					display: 'block'
+				})
+			}).then(function () {
+				_this.oRoot.addClass('is-show');
+			}).do();
+		},
+
+		close: function () {
+			var _this = this;
+			new Delay(function () {
+				_this.oRoot.removeClass('is-show');
+			}).then(300, function () {
+				_this.oRoot.remove();
+			}).do();
+		}
+	}
+
+
+	window.Alert = Alert;
+})(window, document);
---
+++ plugIns/0003_对话框/tools/confirm/confirm.js
@@ -0,0 +1,126 @@
+(function (window, document) {
+
+	function Confirm() {
+		var arg1 = arguments[0];
+		var arg2 = arguments[1];
+		var arg3 = arguments[2];
+
+		var ty1 = typeof arg1;
+		var ty2 = typeof arg2;
+		var ty3 = typeof arg3;
+
+		if (!arg1) {
+			return;
+		}
+
+		if (ty2 == 'object') {
+			arg3 = arg2;
+		}
+
+		if (ty2 != 'object' && ty3 != 'object') {
+			arg3 = {}
+		}
+
+		if (ty2 != 'string') {
+			arg2 = '提示';
+		}
+
+		this._init(arg1, arg2, arg3);
+	}
+
+	Confirm.prototype = {
+		_init: function (msg, title, config) {
+			this.msg = msg;
+			this.title = title;
+			this.config = config;
+
+			this.oRoot = null;		// 根节点
+			this.oContent = null;	// 显示内容
+			this.oConfrim = null;	// 确定按钮
+			this.oCancel = null;	// 取消按钮
+
+			this._initParam();
+			this._initStructure();
+			this._initEvent();
+			this.show();
+		},
+
+		_initEvent: function () {
+			var _this = this;
+
+			// 背景点击
+			this.oRoot.onclick = function () {
+				_this.close();
+			}
+
+			// 内容点击
+			this.oContent.onclick = function (evt) {
+				var e = evt || window.event;
+				evt ? e.stopPropagation() : e.cancelBubble = true;
+			}
+
+			// 确定按钮点击
+			this.oConfirm.onclick = function () {
+				if (_this.config.confirmFn) {
+					_this.config.confirmFn.apply(_this);
+				} else {
+					_this.close();
+				}
+			}
+
+			// 取消按钮点击
+			this.oCancel.onclick = function () {
+				if (_this.config.cancelFn) {
+					_this.config.cancelFn.apply(_this);
+				} else {
+					_this.close();
+				}
+			}
+		},
+
+		_initParam: function () {
+			this.confirmText = this.config.confirmText || '确定';
+			this.cancelText = this.config.cancelText || '取消';
+		},
+
+		_initStructure: function () {
+			this.oRoot = document.createElement('div');
+			this.oRoot.className = 'confirm-box _wrapper';
+			this.oRoot.innerHTML = `<div class="_content">
+					<div class="_header">${this.title}</div>
+					<div class="_message"><p>${this.msg}</p></div>
+					<div class="_footer">
+						<button class="_cancel-btn">${this.cancelText}</button>
+						<button class="_confirm-btn">${this.confirmText}</button>
+					</div>
+				</div>`;
+			this.oContent = this.oRoot.querySelector('._content');
+			this.oCancel = this.oRoot.querySelector('._cancel-btn');
+			this.oConfirm = this.oRoot.querySelector('._confirm-btn');
+			document.body.appendChild(this.oRoot);
+		},
+
+		show: function () {
+			var _this = this;
+			new Delay(function () {
+				tools.setStyle(_this.oRoot, {
+					display: 'block'
+				})
+			}).then(function () {
+				tools.addClass(_this.oRoot, 'is-show');
+			}).do();
+		},
+
+		close: function () {
+			var _this = this;
+			new Delay(function () {
+				tools.delClass(_this.oRoot, 'is-show');
+			}).then(300, function () {
+				document.body.removeChild(_this.oRoot);
+			}).do();
+		}
+	}
+
+
+	window.Confirm = Confirm;
+})(window, document);
