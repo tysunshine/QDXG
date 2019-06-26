ab9fdccb4f56744f490025092a2d1f5b23def7d8
tianyuan <2422317862@qq.com>
2019-06-26 21:01:18 +0800

    滚动条样式 以及 表单输入框验证

---
+++ jsFunction/008_表单/my-check-form.js
@@ -0,0 +1,140 @@
+/**
+ * @my-check-form.js
+ * @作者 田园
+ * @版本 1.0.1
+ * @Created: 19-06-26
+ */
+(function (window, document) {
+	/**
+     * 输入框验证方法
+     * 支持jQuery $全局调用以及模块化调用
+     * @example
+     * 初始化
+     * $.MyCheckForm.init({
+     *		class: 'my-check-item', 			// 默认class: 'my-check-item'
+     *		rules: {
+	 *			id1: [{
+	 *				type: 'blur',				// 默认type:'blur'
+	 *				fn: function (val) {
+	 *					if (val.length == 0) {
+	 *						return '不能为空'；
+	 *					}
+	 *					return '';
+	 *				}
+	 *			}]
+     *		}
+     *	});
+     * 
+     * 按钮验证
+     * console.log($.MyCheckForm.validate());	// 结果true/false
+     **/
+
+	var MyCheckForm = (function () {
+
+		var oDeploy = null;
+		var oCheckList = null;
+
+		var initState = function () {
+			oCheckList = {};
+			for (var id in oDeploy.rules) {
+
+				var oId = $('#' + id);
+
+				oDeploy.rules[id].forEach(function (item, idx, arr) {
+					if (!item.type) {
+						arr[idx].type = 'blur';
+					}
+				})
+
+				if (oId.length > 0) {
+					oCheckList[id] = oId;
+				}
+			}
+		}
+
+		// 绑定事件
+		var initEvent = function () {
+			for (var id in oCheckList) {
+
+				oDeploy.rules[id].forEach(function (item, idx, arr) {
+					oCheckList[id].on(item.type, function () {
+						setErrorType($(this), item.fn);
+					})
+				})
+			}
+		}
+
+
+		var setErrorType = function (oTag, fn) {
+			var oTip = oTag.prev();
+			var oBox = oTag.parent();
+			var val = oTag.val();		// 当前输入框值
+			var msg = fn(val);			// 错误消息
+
+			
+
+			if (msg) {
+				oTip.text(msg);
+				oTip.fadeIn(300)
+				oBox.animate({
+					marginBottom: '20px'
+				}, 300);
+
+			} else {
+				oTip.fadeOut(300)
+				oBox.animate({
+					marginBottom: '0'
+				}, 300);
+			}
+
+			return msg ? false : true;
+		}
+
+
+		return {
+			init: function (o) {
+				if (!o) {
+					return;
+				}
+				if (!o.class) {
+					o.class = 'my-check-item';
+				}
+				if (!o.rules) {
+					return;
+				}
+
+				oDeploy = o;
+
+
+				initState();
+				initEvent();
+
+			},
+
+			validate: function () {
+				var bl = true;
+				if (!oCheckList) {
+					return false;
+				}
+				for (var id in oCheckList) {
+					oDeploy.rules[id].forEach(function (item, idx, arr) {
+						var res = setErrorType(oCheckList[id], item.fn);
+						if (bl && !res) {
+							bl = false;
+						}
+					})
+				}
+
+				return bl;
+			}
+		}
+	})()
+
+	$.MyCheckForm = MyCheckForm;
+
+	if (typeof define === 'function' && (define.amd || define.cmd)) {
+        define(MyCheckForm);
+    } else {
+        window.MyCheckForm = MyCheckForm;
+    }
+})(window, document);
