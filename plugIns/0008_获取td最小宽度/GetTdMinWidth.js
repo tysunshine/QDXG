b0da1766a08b9fc371f6febf10bd68e45036b947
tianyuan <877430318@qq.com>
2019-09-27 16:23:50 +0800

    td最小宽度、文本溢出

---
+++ plugIns/0008_获取td最小宽度/GetTdMinWidth.js
@@ -0,0 +1,146 @@
+;(function (window, document) {
+	var oGetTdMinWidth = {
+		config: {
+			fontSize: 14,	// 字体大小
+			distance: 10,	// 预留间隔
+			min: 90,		// 最小宽度
+			max: 250,		// 最大宽度
+			pl: 10,			// padding-left
+			pr: 10,			// padding-right
+		},
+		key: {},		// 每个key的具体配置
+
+		aMinWidth: [], // 最小值数组，和head数组一一对应
+		timer: null, // 用于移除计算文本最小宽度的元素
+		isEnter: false, // 是否进入了计算，10秒没进入就移除元素
+
+		init: function (tableData, config) {
+
+			if (!tableData) {
+				return;
+			}
+
+			if (!config) {
+		        config = {}
+		    }
+
+		    this.tableData = tableData;
+
+		    // 字体
+		    this.config.fontSize = config.fontSize || this.config.fontSize;
+
+		    // 间隔
+		    this.config.distance = config.distance || this.config.distance;
+
+		    // 最小值，可谓auto
+		    this.config.min = config.min || this.config.min;
+
+		    // 最大值，可为auto
+		    this.config.max = config.max || this.config.max;
+
+		    // padding-left
+		    this.config.pl = (config.pl > 0 || (typeof config.pl == 'number' && !config.pl)) ? config.pl : this.config.pl;
+
+		    // padding-right
+		    this.config.pr = (config.pr > 0 || (typeof config.pr == 'number' && !config.pr)) ? config.pr : this.config.pr;
+
+		    // 固定属性的最小宽度
+		    this.key = this.getKey(config.key || {});
+
+		    this.aMinWidth = [];
+		    this.basisHeadMin(); // 根据head设置最小值
+		    this.basisDataMin(); // 根据data设置最小值
+
+		    return this.aMinWidth;
+		},
+
+		// 获取每个key的具体配置
+		getKey: function (obj) {
+			var res = {}
+
+			for (var i = 0; i < this.tableData.head.length; i++) {
+				var key = this.tableData.head[i].key;
+				res[key] = JSON.parse(JSON.stringify(this.config));
+
+				for ( var j in res[key]) {
+					if (obj[key] && obj[key][j]) {
+						res[key][j] = obj[key][j];
+					}
+				}
+			}
+
+			return res;
+		},
+
+		// 根据head设置最小值
+		basisHeadMin: function () {
+			for (var i = 0; i < this.tableData.head.length; i++) {
+				var key = this.tableData.head[i].key;
+				var text = this.tableData.head[i].name;
+				var width = this.basisTextMin(key, text);
+
+				this.aMinWidth.push(width);
+			}
+		},
+
+		// 根据data设置最小值
+		basisDataMin: function () {
+			for (var i = 0; i < this.tableData.data.length; i++) {
+				for (var j = 0; j < this.tableData.head.length; j++) {
+					var key = this.tableData.head[j].key;
+					var text = this.tableData.data[i][key];
+
+					var width = this.basisTextMin(key, text);
+
+					if (this.aMinWidth[j] < width) {
+						this.aMinWidth[j] = width;
+					}
+				}
+			}
+		},
+
+		// 根据文本判断最小值
+		basisTextMin: function (key, text) {
+			var _this = this;
+			var oKey = this.key[key]; // key项的配置
+
+			var oCalc = document.getElementById('_calcMinWidthSpan');
+	        if (!oCalc) {
+	            oCalc = document.createElement('span');
+	            oCalc.id = '_calcMinWidthSpan';
+	            oCalc.style.visibility  = 'hidden';
+	            oCalc.style.display = 'inline-block';
+	            oCalc.style.whiteSpace = 'nowrap';
+	            oCalc.style.position = 'absolute';
+	            document.body.appendChild(oCalc);
+
+	            clearInterval(this.timer);
+	            this.timer = setInterval(function () {
+	            	if (!_this.isEnter) {
+	            		document.body.removeChild(oCalc);
+						clearInterval(_this.timer);
+	            	}
+	            	_this.isEnter = false;
+	            }, 10000)
+	        }
+	        
+	        oCalc.style.fontSize = oKey.fontSize + 'px';
+	        oCalc.innerText = text;
+	        this.isEnter = true;
+
+	        var res = oCalc.clientWidth + oKey.pl + oKey.pr + oKey.distance;
+	        if (oKey.min != 'auto' && res < oKey.min) {
+	            res = oKey.min;
+	        }
+	        if (oKey.max != 'auto' && res > oKey.max) {
+	            res = oKey.max;
+	        } 
+
+	        return res;
+		}
+	}
+
+	window.GetTdMinWidth = function (tableData, config) {
+		return oGetTdMinWidth.init(tableData, config);
+	}
+})(window, document);
---
+++ plugIns/0009_文字溢出提示/hoverTip.js
@@ -0,0 +1,75 @@
+/**
+ * 文本溢出提示
+ * 参数：el -> 需要绑定该动作的选择器，默认.hover-tip
+ */
+;(function (window, document) {
+	var oHoverTip = {
+		init: function (el) {
+			this.oTip = document.querySelectorAll(el);	
+			this.initEvent();
+		},
+		initEvent: function () {
+			for (var i = 0; i < this.oTip.length; i++) {
+				var oThis = this.oTip[i];
+
+				if (oThis.bindTip) {
+					continue;
+				}
+
+				oThis.bindTip = true;
+				var timer = null, isEnter = false;
+				oThis.onmouseenter = function () {
+					var iTw = this.clientWidth;
+					var iPl = parseInt(getComputedStyle(this, null)['paddingLeft']);
+					var iPr = parseInt(getComputedStyle(this, null)['paddingRight']);
+					var iSize =  getComputedStyle(this, null)['fontSize'];
+					var strText = this.innerText;
+					iTw = iTw - iPl - iPr;
+
+					isEnter = true;
+
+					var oCalc = document.getElementById('_calcMinWidthSpan');
+					if (!oCalc) {
+						oCalc = document.createElement('div');
+						oCalc.id = '_calcMinWidthSpan';
+						oCalc.style.display = 'inline-block';
+						oCalc.style.whiteSpace = 'nowrap';
+						oCalc.style.visibility  = 'hidden';
+						oCalc.style.position = 'absolute';
+						document.body.appendChild(oCalc);
+						
+						clearInterval(timer);
+						timer = setInterval(function () {
+							if (!isEnter) {
+								document.body.removeChild(oCalc);
+								clearInterval(timer);
+							}
+							isEnter = false;
+						}, 10000);
+					}
+						
+					oCalc.style.fontSize = iSize;
+					oCalc.innerText = strText;
+					
+					var iCw = oCalc.clientWidth;
+
+					if (iCw > iTw) {
+						this.setAttribute('title', strText);
+					}
+				}
+				oThis.onmouseleave = function () {
+					var oCalc = document.querySelectorAll('._calc-hover-tip');
+					this.removeAttribute('title');
+					for (var i = 0; i < oCalc.length; i++) {
+						document.body.removeChild(oCalc[i]);
+					}
+				}
+			}
+		}
+	}
+
+	window.hoverTip = function (el) {
+		this.el = el || '.hover-tip';
+		oHoverTip.init(this.el);
+	}
+})(window, document);
